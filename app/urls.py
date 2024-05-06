79cdbf2135b226f19b6c57c6bdecdb732a8ce39b
Adilkhan Altynbek <adilkhanaltynbek@MacBook-Pro-Adilkhan.local>
2024-05-06 17:57:05 +0500

    UPDATE

--- app/urls.py
+++ app/urls.py
@@ -4,23 +4,16 @@ from . import views
 
 urlpatterns = [
     path("", views.handleregistration, name='registration'),
-    path("ind", views.index, name='index'),
-    path("about/", views.about, name='about'),
-    path("contact/", views.contact, name='contact'),  # Added comma here
-    path("login/", views.handlelogin, name='login'),  # Added comma here
-    path("registration/", views.handleregistration, name='registration'),  # Comma added here
-    path("translator/", views.translator_home, name='translator_home'),
-    path("project-manager/", views.project_manager, name='project_manager_home'),
-    path("chief-editor/", views.chief_editor_home, name='chief_editor_home'),
+# Login-Registration
     path("success/", views.successful_register, name='successful_register'),
-    path('project/update/<int:project_id>/', views.update_project, name='update_project'),
-    path('project-manager/<int:project_id>/', views.project_manager, name='project_manager'),
-    path('project-manager/', views.project_manager, name='project_manager'),
-    path('project-detail/<int:project_id>/', views.project_manager_detail, name='project_manager_detail'),
+    path("login/", views.handlelogin, name='login'),  
+    path("registration/", views.handleregistration, name='registration'),  
+
+# Workers main pages
+    path("translator/", views.translator_home, name='translator_home'),
+    path("chief-editor/", views.chief_editor_home, name='chief_editor_home'),
 
 
-    path('translator/<int:project_id>/', views.project_detail, name='project_detail'),
-    path('chief-editor/<int:project_id>/', views.project_detail_2, name='project_detail_2'),
 
     path('update_activity/<int:activity_id>/', views.update_activity, name='update_activity'),
     path('update_activity_2/<int:activity_id>/', views.update_activity_2, name='update_activity_2'),
--- app/views.py
+++ app/views.py
@@ -10,14 +10,6 @@ from django.http import HttpResponse
 from django.template import loader
 from django.urls import reverse
 
-def index(request):
-    return render(request, 'index.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def contact(request):
-    return render(request, 'contact.html')
 
 
 def handlelogin(request):
@@ -44,7 +36,8 @@ def handlelogin(request):
         form = CustomAuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
-    
+
+
 def handleregistration(request):
         if request.method == 'POST':
             form = UserRegistrationForm(request.POST)
@@ -52,25 +45,14 @@ def handleregistration(request):
                 username = form.cleaned_data['username']
                 password = form.cleaned_data['password1']
                 user_type = form.cleaned_data['user_type']
-
             # Create a new CustomUser object and save it to the database
                 new_user = CustomUser.objects.create_user(username=username, password=password, user_type=user_type)
-
                 return redirect('successful_register')  # Redirect to the login page after successful registration
         else:
             form = UserRegistrationForm()
         return render(request, 'registration.html', {'form': form})
 
 
-
-def project_detail(request, project_id):
-    project = Project.objects.get(id=project_id)
-    projects = Project.objects.all()  # Получаем список всех проектов
-    return render(request, 'project_detail.html', {'project': project, 'projects': projects})
-def project_detail_2(request, project_id):
-    project = Project.objects.get(id=project_id)
-    projects = Project.objects.all()  # Получаем список всех проектов
-    return render(request, 'project_detail_2.html', {'project': project, 'projects': projects})
 
 
 def translator_home(request):
@@ -91,19 +73,6 @@ def chief_editor_home(request):
     return render(request, 'chief_editor_home.html', context)
 
 
-
-def translator_detail(request):
-    all_projects = Project.objects.all()
-    context = {
-        'projects_detail': all_projects,
-    }
-    return render(request, 'project_detail.html', context)
-def chief_editor_detail(request):
-    all_projects = Project.objects.all()
-    context = {
-        'projects_detail_2': all_projects,
-    }
-    return render(request, 'project_detail_2.html', context)
 
 
 def update_activity(request, activity_id):
@@ -130,60 +99,3 @@ def update_activity_2(request, activity_id):
 def successful_register(request):
     # Your logic for translator home page
     return render(request, 'successful_register.html', )
-
-def project_manager(request):
-    projects = Project.objects.all()
-    user = request.user
-    translators = CustomUser.objects.filter(user_type='translator')
-
-    if request.method == 'POST':
-        form = ProjectForm(request.POST)
-        if form.is_valid():
-            project = form.save(commit=False)
-            project.selected_translator = user
-            project.save()
-            return redirect('project_manager_home')  
-    else:
-        form = ProjectForm()
-
-    context = {
-        'projects': projects,
-        'user': user,
-        'form': form,
-        'translators': translators
-    }
-    return render(request, 'project_manager_home.html', context)
-
-def update_project(request, project_id):
-    project = get_object_or_404(Project, id=project_id)
-    if request.method == 'POST':
-        form = ProjectForm(request.POST, instance=project)
-        if form.is_valid():
-            form.save()
-            return redirect('project_manager_home')
-    else:
-        form = ProjectForm(instance=project)
-    return render(request, 'edit_project.html', {'form': form})
-
-def project_manager_detail(request, project_id):
-    project = get_object_or_404(Project, id=project_id)
-    activities = Activity.objects.filter(project=project)
-
-    if request.method == 'POST':
-        form = ActivityForm(request.POST)
-        if form.is_valid():
-            activity = form.save(commit=False)
-            activity.project = project
-            activity.save()
-            return redirect('project_manager_detail', project_id=project_id)
-    else:
-        form = ActivityForm()
-
-    context = {
-        'project': project,
-        'activities': activities,
-        'form': form,
-    }
-    return render(request, 'project_manager_detail.html', context)
-
-
